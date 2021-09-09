e45f37ee115330d179a9c8283ed4d2582a9264d5
Heather Dalton <79050392+hjdalton@users.noreply.github.com>
2021-09-09 09:30:05 +0100

    cypress sign up test includes new fields

--- cypress/integration/user_can_log_in_spec.js
+++ cypress/integration/user_can_log_in_spec.js
@@ -2,8 +2,8 @@ describe('Login', function() {
   it('allows the user to log in', function() {
     cy.visit('/login');
         
-    cy.get('#login-form').find('[type="text"]').type('jimmyboi');
-    cy.get('#login-form').find('[type="password"]').type('1234');
+    cy.get('#login-form').find('[id="username"]').type('jimmyboi');
+    cy.get('#login-form').find('[id="password"]').type('1234');
         
     cy.get('#login-form').submit();
   });
